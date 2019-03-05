5c3d14443a2afe1bb53c4d42da57372c59c7df73
foreverlms <codechaser@163.com>
2019-03-05 23:53:11 +0800

    leetcode20 solution of java.栈的应用，但内存占用较大，需要优化

--- java/src/com/bob/Solution.java
+++ java/src/com/bob/Solution.java
@@ -1,6 +1,20 @@
 package com.bob;
 
+import java.util.Stack;
+
+/**
+ * @author :bobliao
+ * @description :Leetcode solutions in Java.
+ * @version: :v1.0
+ * @date :Created in 19-3-4 下午11:06
+ */
+
 public class Solution {
+    /**
+     * Leetcode 64 动态规划
+     * @param grid
+     * @return
+     */
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
@@ -22,5 +36,29 @@ public class Solution {
         }
 
         return sum[m-1][n-1];
+    }
+
+    /**
+     * Leetcode 20 栈的用法，每次都把{,(,[压入栈内，碰到},),]就和栈顶的元素比较，是否配对
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s) {
+        int len = s.length();
+        if (len == 0)
+            return true;
+        if (len % 2 == 1)
+            return false;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0;i<len;++i){
+            char tmp = s.charAt(i);
+            if (tmp == '{' || tmp == '(' || tmp == '['){
+                stack.push(tmp);
+            }
+            else if (stack.isEmpty() || (tmp == '}' && stack.pop() != '{') || (tmp == ']' && stack.pop() != '[') || (tmp == ')' && stack.pop() != '('))
+                return false;
+        }
+
+        return stack.isEmpty();
     }
 }
